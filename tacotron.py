ce9df6f39711707d1d003f3a6875ebc24bf8d6e9
Gabe Villasana <villagab4@gmail.com>
2019-12-18 14:20:13 -0500

    Updated model functions

--- tacotron.py
+++ tacotron.py
@@ -20,10 +20,12 @@ def tacotron(n_mels, r, k1, k2, nb_char_max,embedding_size, mel_time_length,mag_
 
     # Decoder-part1-Prenet:
     input_decoder = Input(shape=(None, n_mels))
-    attention_rnn_output = get_attention_RNN()(get_pre_net(input_decoder))
+
+    # Attention RNN
+    attention_rnn_output = GRU(256)(get_pre_net(input_decoder))
 
     # Attention
-    attention_context = get_attention_context(get_CBHG_encoder(prenet_encoding,k1),
+    attention_context = get_attention_context(CBHGEncoder(prenet_encoding,k1),
                                               RepeatVector(nb_char_max)(attention_rnn_output))
 
     context_shapes = (int(attention_context.shape[1]), int(attention_context.shape[2]))
@@ -33,7 +35,7 @@ def tacotron(n_mels, r, k1, k2, nb_char_max,embedding_size, mel_time_length,mag_
     input_of_decoder_rnn = concatenate([attention_context, attention_rnn_output_reshaped])
     input_of_decoder_rnn_projected = Dense(256)(input_of_decoder_rnn)
 
-    output_of_decoder_rnn = get_decoder_RNN_output(input_of_decoder_rnn_projected)
+    output_of_decoder_rnn = decoderRNNOutput(input_of_decoder_rnn_projected)
 
     mel_hat = Reshape((mel_time_length, n_mels * r))(Dense(mel_time_length * n_mels * r)(output_of_decoder_rnn))
 
@@ -42,7 +44,7 @@ def tacotron(n_mels, r, k1, k2, nb_char_max,embedding_size, mel_time_length,mag_
         return x[:, :, -n_mels:]
 
     mel_hat_last_frame = Lambda(slice)(mel_hat)
-    post_process_output = get_CBHG_post_process(mel_hat_last_frame,k2)
+    post_process_output = CBHGPostProcess(mel_hat_last_frame,k2)
 
     z_hat = Reshape((mag_time_length, (1 + n_fft // 2)))(Dense(mag_time_length * (1 + n_fft // 2))(post_process_output))
 
@@ -60,7 +62,7 @@ def get_pre_net(input_data):
     prenet = Dropout(0.5)(prenet)
     return prenet
 
-def get_conv1dbank(kernel_sizes, input_data):
+def get_conv1dstack(kernel_sizes, input_data):
     convolution = Conv1D(filters=128, kernel_size=1,
                   strides=1, padding='same')(input_data)
     convolution = BatchNormalization()(convolution)
@@ -91,8 +93,8 @@ def get_highway_output(highway_input, nb_layers, activation="tanh", bias=-3):
     return highway_output
 
 
-def get_CBHG_encoder(input_data, K_CBHG):
-    conv1dbank = get_conv1dbank(K_CBHG, input_data)
+def CBHGEncoder(input_data, K_CBHG):
+    conv1dbank = get_conv1dstack(K_CBHG, input_data)
     conv1dbank = MaxPooling1D(pool_size=2, strides=1,
                               padding='same')(conv1dbank)
     conv1dbank = Conv1D(filters=128, kernel_size=3,
@@ -114,8 +116,8 @@ def get_CBHG_encoder(input_data, K_CBHG):
     return CBHG_encoder
 
 
-def get_CBHG_post_process(input_data, K_CBHG):
-    conv1dbank = get_conv1dbank(K_CBHG, input_data)
+def CBHGPostProcess(input_data, K_CBHG):
+    conv1dbank = get_conv1dstack(K_CBHG, input_data)
     conv1dbank = MaxPooling1D(pool_size=2, strides=1,
                               padding='same')(conv1dbank)
     conv1dbank = Conv1D(filters=256, kernel_size=3,
@@ -125,9 +127,6 @@ def get_CBHG_post_process(input_data, K_CBHG):
     conv1dbank = Conv1D(filters=80, kernel_size=3,
                         strides=1, padding='same')(conv1dbank)
     conv1dbank = BatchNormalization()(conv1dbank)
-
-    # residual learning helps training deep networks
-    # (https://arxiv.org/pdf/1512.03385.pdf)
     residual = Add()([input_data, conv1dbank])
 
     highway_net = get_highway_output(residual, 4, activation='relu')
@@ -136,25 +135,12 @@ def get_CBHG_post_process(input_data, K_CBHG):
 
     return CBHG_post_proc
 
-# see https://arxiv.org/pdf/1609.08144.pdf (stack of GRUs with vertical
-# residual connections)
 
-
-def get_decoder_RNN_output(input_data):
-
-    rnn1 = GRU(256, return_sequences=True)(input_data)
-
-    inp2 = Add()([input_data, rnn1])
-    rnn2 = GRU(256)(inp2)
-
-    decoder_rnn = Add()([inp2, rnn2])
-
-    return decoder_rnn
-
-
-def get_attention_RNN():
-    return GRU(256)
-
+def decoderRNNOutput(input_data):
+    stuff_to_add = [input_data, GRU(256, return_sequences=True)(input_data)]
+    inp2 = Add()(stuff_to_add)
+    stuff_to_add_2 = [inp2, GRU(256)(inp2)]
+    return Add()(stuff_to_add_2)
 
 def get_attention_context(encoder_output, attention_rnn_output):
     attention_input = Concatenate(axis=-1)([encoder_output,
@@ -162,7 +148,5 @@ def get_attention_context(encoder_output, attention_rnn_output):
     e = Dense(10, activation="tanh")(attention_input)
     energies = Dense(1, activation="relu")(e)
     attention_weights = Activation('softmax')(energies)
-    context = Dot(axes=1)([attention_weights,
+    return Dot(axes=1)([attention_weights,
                            encoder_output])
-
-    return context
